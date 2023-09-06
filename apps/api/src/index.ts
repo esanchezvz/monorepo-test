fcdac85b1dbadfb26261a1a80e3afa733ae253d8
Esteban Sánchez <esteban.sanvaz@gmail.com>
2023-09-06 17:49:39 -0600

    feat: connects trpc package with api app

---
+++ apps/api/src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import { appRouter, createExpressMiddleware, createTRPCContext } from '@form/trpc'
+
+const app = express()
+
+// Express middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use((req, _res, next) => {
+  // request logger
+  console.log('↔️ ', req.method, req.path, req.body ?? req.query)
+
+  return next()
+})
+app.use((req, _res, next) => {
+  console.log('[Express Middleware]: ', req.body)
+  return next()
+})
+
+// TRPC
+app.use(
+  '/api',
+  createExpressMiddleware({
+    router: appRouter,
+    createContext: createTRPCContext,
+  }),
+)
+
+// Runs api
+app.listen(4000, () => {
+  console.log('TRPC API listening on port 4000')
+})
